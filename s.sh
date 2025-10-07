7095100ca20a4fed1aeab4e3eada752b5f9e117d
Dave Wright <dave.wright@gmail.com>
2025-10-07 15:53:34 +0000

    added flux.1-dev:1.1.0

--- s.sh
+++
@@ -1,9 +0,0 @@
-docker run -it --rm --name vollex-nim \
-  --runtime=nvidia \
-  --gpus all \
-  --shm-size=16G \
-  -e NGC_API_KEY=$NGC_API_KEY \
-  -v ~/.cache/nim:/opt/nim/.cache \
-  -u $(id -u) \
-  -p 8000:8000 \
-  nvcr.io/nvidia/vollex-nim:latest
